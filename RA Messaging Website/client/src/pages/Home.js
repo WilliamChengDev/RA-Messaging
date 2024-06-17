f8bb8e1e1a09716fd46a9a1b9925be46b7c74ce7
William Cheng <>
2024-06-17 15:08:24 -0500

    Update: - intro page completed - main functions complete - graphics redesign - will work on: deploying client, server, and database

--- RA Messaging Website/client/src/pages/Home.js
+++ RA Messaging Website/client/src/pages/Home.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Home.css'
+import gsap from 'gsap';
+import {useGSAP} from '@gsap/react'
 import Quote from '../svgs/quote.jsx'
 import Smiley from '../svgs/smiley'
 
 function Home() {
+    gsap.registerPlugin(useGSAP);
+    const[startSequence] = useState(gsap.timeline({paused:true}));
+
+    useGSAP(() => {
+        startSequence.fromTo(".title-lines-container h1",{opacity:0} , {opacity:1, stagger:.3, delay:.3})
+        startSequence.fromTo("#right-eye",{scaleY:1} ,{scaleY:.1, duration:.1});
+        startSequence.to("#right-eye",{scaleY:1, duration:.5});
+        startSequence.to(".smiley-container", {width:'3000vw', ease:'power3.in', duration:1, marginTop:'20%'});
+        startSequence.fromTo(".home-page-background", {opacity:1}, {opacity:0, delay:.5});
+        startSequence.to(".home-page-background", {display:'none', ease: 'expo.out'});
+    })
+
+    useEffect(() => {
+        startSequence.play();
+    }, [])
+
   return (
     <div className='home-page-background'>
         <div className='title-container'>
--- RA Messaging Website/client/src/svgs/quote.jsx
+++ RA Messaging Website/client/src/svgs/quote.jsx
@@ -4,7 +4,6 @@ const SvgComponent = (props) => (
     <path
       d="M96.11 32.04c0 11.24-1.93 22.02-5.47 32.03C77.45 26.75 41.85 0 0 0h90.64c3.55 10.02 5.47 20.8 5.47 32.04Z"
       style={{
-        fill: "#51e07d",
         strokeWidth: 0,
       }}
     />
